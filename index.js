14223911867d92fbe8e79d4495cb3842531f0a7b
Nehul Jain <129666061+whonehuljain@users.noreply.github.com>
2023-09-01 01:39:22 +0530

    Adding initial files of the calculator.

---
+++ index.js
@@ -0,0 +1,23 @@
+var display = document.querySelector(".display");
+var buttons = document.querySelectorAll("button");
+var specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
